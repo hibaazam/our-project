702ea846d4cd94657d2cdcbf178e20ca16730aa7
Hiba Azam <hibaazam7@gmail.com>
2018-10-07 01:03:24 +0500

    importing project

---
+++ PhonBook2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhonBook2.Startup))]
+namespace PhonBook2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
